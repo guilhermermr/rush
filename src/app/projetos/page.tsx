d93a3aa8f149bef01fddd9132723c19e31bb10c9
Guilherme Moura <guilhermermribeiro@gmail.com>
2025-10-16 00:38:02 -0300

    inicio estruturacao

---
+++ src/app/projetos/page.tsx
@@ -0,0 +1,5 @@
+const Projetos = () => {
+  return <h2 className="text-2xl text-green-500">About Page</h2>;
+};
+
+export default Projetos;
---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+const Header = () => {
+  return (
+    <header className="bg-blue-500 p-4 text-white font-bold">Header</header>
+  );
+};
+
+export default Header;
