ce3a84da62e4d61b55fe4044703d61ecc42de6ba
Guilherme Moura <guilhermermribeiro@gmail.com>
2025-10-19 00:40:21 -0300

    cria component login

---
+++ src/app/login/page.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import Header from "@/components/Header";
+import { use, useState } from "react";
+
+const login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handlerLogin = (e: any) => {
+    e.preventDefault();
+    console.log("Email:", email);
+    console.log("Password:", password);
+  };
+
+  return (
+    <>
+      <Header />
+
+      <div className="container mx-auto">
+        <div className="row">
+          <div className="col-md-6 offset-md-3">
+            <h2 className="text-center my-4">Login</h2>
+            <form onSubmit={handlerLogin}>
+              <div className="mb-3">
+                <label htmlFor="email" className="form-label">
+                  Email
+                </label>
+                <input
+                  type="email"
+                  className="form-control ml-5 border-b-2 border-gray-300 focus-visible:outline-none"
+                  id="email"
+                  name="email"
+                  value={email}
+                  required
+                  onChange={(e) => setEmail(e.target.value)}
+                  placeholder="Enter email"
+                />
+              </div>
+              <div className="mb-3">
+                <label htmlFor="password" className="form-label">
+                  Password
+                </label>
+                <input
+                  type="password"
+                  className="form-control ml-5 border-b-2 border-gray-300 focus-visible:outline-none"
+                  id="password"
+                  name="password"
+                  value={password}
+                  required
+                  onChange={(e) => setPassword(e.target.value)}
+                  placeholder="Password"
+                />
+              </div>
+              <button
+                type="submit"
+                className="btn  w-100 bg-slate-500 text-white px-4 py-2 rounded-lg hover:bg-slate-700 width-full"
+              >
+                Login
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+export default login;
